20fd9d9328f03010d5dd7b251f66a280df81f686
Ana Letícia de Souza Pinheiro <87092368+AnaSouzaPinheiro@users.noreply.github.com>
2023-10-12 13:07:02 -0300

    Update menu.js

--- SCRIPT/menu.js
+++ SCRIPT/menu.js
@@ -2,9 +2,7 @@ const menuCelular = document.querySelector(".menuCelular");
 const hideMenu = document.querySelector(".hideMenu");
 let visivel = false;
 
-menuCelular.addEventListener("click", function (event) {
-  event.stopPropagation();
-
+menuCelular.addEventListener("click", function () {
   if (!visivel) {
     hideMenu.style.display = "block";
   } else {
